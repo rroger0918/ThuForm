954c0b60a34889a627dbd53ae649817b090139f3
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2021-12-12 03:47:44 +0800

    push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ThuForm.Models;
+
+namespace ThuForm.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        FormEntities db = new FormEntities();
+        //int pageSize = 5;
+        // GET: Home
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var registform = db.RegistForm.ToList();
+            //int currentPage = page < 1 ? 1 : page;
+            //var products = db.RegistForm.OrderBy(m => m.DataID).ToList();
+            //var result = products.ToPagedList(currentPage, pageSize);
+            return View(registform);
+        }
+
+        [Authorize]
+        public ActionResult ManagePage()
+        {
+            var registform2 = db.RegistForm.ToList();
+            //int currentPage = page < 1 ? 1 : page;
+            //var products2 = db.RegistForm.OrderBy(m => m.DataID).ToList();
+            //var result = products2.ToPagedList(currentPage, pageSize);
+            return View(registform2);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(RegistForm registForm)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Error = false;
+                var temp = db.RegistForm.Where(m => m.DataID == registForm.DataID)
+                    .FirstOrDefault();
+                if (temp != null)
+                {
+                    ViewBag.Error = true;
+                    return View(registForm);
+                }
+                db.RegistForm.Add(registForm);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registForm);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int DataID)
+        {
+            var employee = db.RegistForm
+                .Where(m => m.DataID == DataID).FirstOrDefault();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RegistForm registForm)
+        {
+            if (ModelState.Count > 0)
+            {
+                var temp = db.RegistForm
+                    .Where(m => m.DataID == registForm.DataID)
+                    .FirstOrDefault();
+                temp.Id = registForm.Id;
+                temp.Name = registForm.Name;
+                temp.Total = registForm.Total;
+                temp.Mail = registForm.Mail;
+                temp.Note = registForm.Note;
+                //temp.isActive = employee.isActive;
+                db.SaveChanges();
+                return RedirectToAction("ManagePage");
+            }
+            return View(registForm);
+        }
+
+        [Authorize]
+        public ActionResult Delete(int DataId)
+        {
+            var employee = db.RegistForm
+                .Where(m => m.DataID == DataId).FirstOrDefault();
+            db.RegistForm.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("ManagePage");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();   // 登出
+            return RedirectToAction("Index");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View("");
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string txtUid, string txtPwd)
+        {
+            string[] uidAry = new string[] { "thu1314" };
+            string[] pwdAry = new string[] { "im1314" };
+
+            // 循序搜尋法
+            int index = -1;
+            for (int i = 0; i < uidAry.Length; i++)
+            {
+                if (uidAry[i] == txtUid && pwdAry[i] == txtPwd)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                ViewBag.Err = "帳密錯誤!";
+            }
+            else
+            {
+                // 表單驗證服務，授權指定的帳號
+                FormsAuthentication.RedirectFromLoginPage(txtUid, true);
+                return RedirectToAction("ManagePage");
+            }
+            return View();
+        }
+    }
+}
---
+++ Scripts/countdown.js
@@ -0,0 +1,27 @@
+var countDownDate = new Date("January 15, 2022 12:30:30").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + " 天 " + hours + " 時 " +
+        minutes + " 分 " + seconds + " 秒 ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
